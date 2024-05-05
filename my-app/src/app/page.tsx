6ea38d46544614f68589d16fd6143ae7a7adc307
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-05-05 12:29:49 +0530

    added navlink

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      le lo pudina
-    </main>
+    <>
+      Welcome Home!
+      <Link href="/blog">Blog</Link>
+    </>
   );
 }
